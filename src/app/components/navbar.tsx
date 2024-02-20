9b59df209fa19d9ecde9aca13879cf19d529315a
muhammadsufiyanbaig <send.sufiyan@gmail.com>
2024-02-20 18:56:55 +0500

    hero and navbar

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -1,7 +1,9 @@
 'use client'
-import Image from "next/image";
-import Link from "next/link";
+import React from "react";
 import { useState } from "react";
+import logo from "../../../public/Assets/Picture/LOGO.png"
+import Image from "../../../node_modules/next/image";
+import Link from "../../../node_modules/next/link";
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -11,112 +13,131 @@ const Navbar = () => {
   };
 
   return (
-    <nav className="lg:bg-gray-100 lg:fixed w-full z-20 top-0 start-0  border-gray-600">
-      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-2">
-        <Link href="https://www.linkedin.com/in/muhammadsufiyanbaig/" target="_blank" className="flex items-center space-x-3 rtl:space-x-reverse">
-          <Image
-            src={require("../../../public/Assets/Picture/LOGO.png")}
-            alt="MSB"
-            height={100}
-            width={100}
-            className="w-[30px] md:w-[50px]"
-          />
-          <span className="self-center text-base md:text-xl font-semibold whitespace-nowrap text-gray-700">
-            Muhammad Sufiyan Baig
-          </span>
-        </Link>
-        <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-          <a href="/Assets/CV/MuhammadSufiyanBaig.pdf" target="_blank" rel="noopener noreferrer">
+    <nav className="bg-gray-800">
+      <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+        <div className="relative flex h-20 items-center justify-between">
+          <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
             <button
               type="button"
-              className="text-gray-700  bg-gray-300 font-medium rounded-lg text-base md:text-lg px-2 py-1 md:px-4 md:py-2 text-center hover:bg-gray-500 hover:text-white"
+              className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+              aria-controls="mobile-menu"
+              aria-expanded="false"
+              onClick={toggleMenu}
             >
-              Resume
-            </button>
-          </a>
-          <button
-            data-collapse-toggle="navbar-sticky"
-            type="button"
-            className="inline-flex items-center sm:p-0 p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden focus:outline-none focus:ring-2 hover:bg-gray-700 focus:ring-gray-600"
-            aria-controls="navbar-sticky"
-            aria-expanded="false"
-            onClick={toggleMenu}
-          >
-            <span className="sr-only">Open main menu</span>
-            <svg
-              className="w-5 h-5"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 17 14"
-            >
-              <path
+              <span className="absolute -inset-0.5" />
+              <span className="sr-only">Open main menu</span>
+              <svg
+                className={`h-6 w-6 ${isOpen ? "block" : "hidden"}`}
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
                 stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M1 1h15M1 7h15M1 13h15"
+                aria-hidden="true"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+              <svg
+                className={`h-6 w-6 ${isOpen ? "hidden" : "block"}`}
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                aria-hidden="true"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+                />
+              </svg>
+            </button>
+          </div>
+          <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+            <div className="flex flex-shrink-0 items-center">
+              <Image
+                className="h-10 w-auto"
+                src={logo}
+                alt="Logo"
               />
-            </svg>
-          </button>
-        </div>
-        <div
-          className={`items-center justify-between w-full md:flex md:w-auto md:order-1 ${isOpen ? "block" : "hidden"
-            }`}
-          id="navbar-sticky"
-        >
-          <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium rounded-lg md:space-x-8 rtl:space-x-reverse justify-center items-center md:flex-row md:mt-0 md:border-0 ">
-            <li>
-              <Link
-                href="/"
-                className="block py-2 px-3 text-black rounded md:bg-transparent  md:p-0 md:text-gray-500"
-                aria-current="page"
-              >
-                Home
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="#about"
-                className="block py-2 px-3 text-black rounded md:bg-transparent  md:p-0 md:text-gray-500"
-                aria-current="page"
-              >
-                About
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="#skills"
-                className="block py-2 px-3 text-black rounded md:bg-transparent  md:p-0 md:text-gray-500"
-                aria-current="page"
-              >
-                Skills
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="#project"
-                className="block py-2 px-3 text-black rounded md:bg-transparent  md:p-0 md:text-gray-500"
-                aria-current="page"
-              >
-                Project
-
-              </Link>
-
-            </li>
-            <li>
-              <Link
-                href="#contact"
-                className="block py-2 px-3 text-black rounded md:bg-transparent md:p-0 md:text-gray-500"
-                aria-current="page"
-              >
-                Contact
-              </Link>
-            </li>
-
-          </ul>
+            </div>
+            <div className="hidden sm:ml-6 sm:block">
+              <div className="flex space-x-4">
+                <Link
+                  href="/"
+                  className="text-white  hover:bg-gray-700 rounded-md px-3 py-2 text-md font-bold"
+                  aria-current="page"
+                >
+                  Home
+                </Link>
+                <Link
+                  href="#about"
+                  className="text-white hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-md font-bold"
+                >
+                  About
+                </Link>
+                <Link
+                  href="#skill"
+                  className="text-white hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-md font-bold"
+                >
+                  Skills
+                </Link>
+                <Link
+                  href="#project"
+                  className="text-white hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-md font-bold"
+                >
+                  Project
+                </Link>
+                <Link
+                  href="#contact"
+                  className="text-white hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-md font-bold"
+                >
+                  Contact
+                </Link>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
+      <div className={`sm:hidden ${isOpen ? "block" : "hidden"}`} id="mobile-menu">
+        <div className="space-y-1 px-2 pb-3 pt-2">
+          <Link
+            href="/"
+            className=" text-white hover:bg-gray-700 block rounded-md px-3 py-2 text-base font-bold"
+            aria-current="page"
+          >
+            Home
+          </Link>
+          <Link
+            href="#about"
+            className="text-white hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-bold"
+          >
+            About
+          </Link>
+          <Link
+            href="#skill"
+            className="text-white hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-bold"
+          >
+            Skills
+          </Link>
+          <Link
+            href="#project"
+            className="text-white hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-bold"
+          >
+            Projects
+          </Link>
+          <Link
+            href="#contact"
+            className="text-white hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-bold"
+          >
+            Contact
+          </Link>
+        </div>
+      </div>
+
     </nav>
   );
 };
