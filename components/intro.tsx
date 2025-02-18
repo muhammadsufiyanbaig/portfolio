44faf0804e5ee9e04d6334441a053c61a2283a0e
muhammadsufiyanbaig <BDA24S001@stu.smiu.edu.pk>
2025-02-18 20:08:45 +0500

    fix profile pic

--- components/intro.tsx
+++ components/intro.tsx
@@ -30,7 +30,7 @@ export default function Intro() {
             }}
           >
             <Image
-              src="/Pic.jpg"
+              src="/Sufiyan.jpg"
               alt="Muhammad Sufiyan Baig"
               width="192"
               height="192"
